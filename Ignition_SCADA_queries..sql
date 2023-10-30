d855036a7b43e2976d6a5837268a7cff4b35a45d
conniesims <connorwsims@gmail.com>
2023-10-29 19:25:35 -0500

    Removed a script

--- Ignition_SCADA_queries..sql
+++ Ignition_SCADA_queries..sql
@@ -29,3 +29,18 @@ select sp.*,
 	   FROM_UNIXTIME(sp.end_time /1000) as endtime
 from sqlth_partitions sp 
 order by start_time desc
+
+--Current day tag data
+SET @currentDate = DATE_FORMAT(CURRENT_DATE(), '%Y%m%d');
+SET @currentTable = CONCAT('sqlt_data_1_', @currentDate);
+
+
+SET @sql = CONCAT('
+					SELECT sd.*,
+						   FROM_UNIXTIME(sd.t_stamp/1000) as readtime 
+					FROM ', @currentTable, ' sd ORDER BY t_stamp desc
+				  ');
+PREPARE dynamic_sql FROM @sql;
+EXECUTE dynamic_sql;
+
+
